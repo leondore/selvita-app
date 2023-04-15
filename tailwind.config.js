0af952dc432a9e6d1ed9bf1ea1bf7fd4d3745d1c
Leon Dore <leon.dore@gmail.com>
2023-04-15 15:29:49 -0400

    Project init

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#6BA054',
+        secondary: '#9AB257',
+        tertiary: '#764B29',
+        accent: '#FFAA33',
+        dark: '#38483E',
+        success: '#4DA858',
+        warning: '#FFBA2D',
+        danger: '#CC2936',
+      },
+      fontFamily: {
+        sans: ['var(--font-poppins)', ...defaultTheme.fontFamily.sans],
+        display: ['var(--font-montserrat)', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [],
+};
